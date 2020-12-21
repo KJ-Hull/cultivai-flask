6175515c7263bb05e75ae487234f6d15105a4d18
KJ-Hull <keegan.j.hull@gmail.com>
2020-12-21 13:42:01 +0200

    21/12/2020

--- src/control.py
+++ src/control.py
@@ -52,7 +52,7 @@ def get_temp(pin):
         if temperature is not None and temperature is not None or time.time() > timeout:
             break
         
-    temp = round(temperature,2)
+    temp = round(humidity,2)
     return temp
 
 def get_humid(pin):
