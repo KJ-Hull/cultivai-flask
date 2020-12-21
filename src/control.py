4938201393f8a348e7f09e1ac6bb8cd5f7e16a44
KJ-Hull <keegan.j.hull@gmail.com>
2020-12-21 13:16:12 +0200

    21/12/2020

--- src/control.py
+++ src/control.py
@@ -34,16 +34,16 @@ temp_hum_pin = 13
 def measure(pin):
     return Adafruit_DHT.read(Adafruit_DHT.DHT11, pin)
 
-#def set_status(pin,status):
-    #GPIO.output(pin, status)
-    #return GPIO.input(pin)
+def set_status(pin,status):
+    GPIO.output(pin, status)
+    return GPIO.input(pin)
 
-#def get_status(pin):
-    #status = GPIO.input(pin)
-    #if status == 1:
-    #    return True
-    #elif status == 0: 
-     #   return False
+def get_status(pin):
+    status = GPIO.input(pin)
+    if status == 1:
+        return True
+    elif status == 0: 
+        return False
 
 def get_temp(pin):
     timeout = time.time() + 1
