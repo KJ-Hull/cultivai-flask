8a882c7d085abc5e79d6e55f7d9e4904d3087c72
KJ-Hull <keegan.j.hull@gmail.com>
2021-01-03 13:01:14 +0200

    03/01/2021

--- src/aws.py
+++ src/aws.py
@@ -43,7 +43,7 @@ def create_json_file(device_id, name, name_json):
     file_name = str(t) + '.json'
     temp_array = '{"device_id":"device_id", "name":"name", "variable":"name"}'
 
-    temp_b = json.loads(name_json)
+    temp_b = json.loads(str(name_json))
     temp_dict = {**temp_array, **temp_b}
 
     with open(file_name, 'w') as outfile:
