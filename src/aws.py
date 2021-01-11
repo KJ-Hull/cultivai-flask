f83c0187bbec3e05c57a2e18da140373da3f2532
KJ-Hull <keegan.j.hull@gmail.com>
2021-01-11 14:12:45 +0200

    11/01/2021

--- src/aws.py
+++ src/aws.py
@@ -6,7 +6,7 @@ import os.path
 from os import path
 s3_resource = boto3.resource('s3')
 
-bucket_name = 'cultivtest_bucket'
+bucket_name = 'cultivtest-bucket'
 
 def check_bucket(bucket_name):
     try:
