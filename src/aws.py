d6a71fe1b3c2de9fd6579dd2041fd8c17bab51fd
KJ-Hull <keegan.j.hull@gmail.com>
2021-01-11 13:41:31 +0200

    11/01/2020-cultivai_aws_test

--- src/aws.py
+++ src/aws.py
@@ -6,7 +6,7 @@ import os.path
 from os import path
 s3_resource = boto3.resource('s3')
 
-bucket_name = 'cultivai-test-bucket'
+bucket_name = 'cultiv.ai-Test_Bucket'
 
 def check_bucket(bucket_name):
     try:
