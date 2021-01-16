519f511dcbecc871a6729c042589952acd6248d8
KJ-Hull <keegan.j.hull@gmail.com>
2021-01-16 10:33:42 +0200

    https-test

--- src/main.py
+++ src/main.py
@@ -119,7 +119,7 @@ def get_uv():
 with app.test_request_context():
    # s3_aws_init(209, "temp", get_temperature())
    endpoint = "acybsaif6qb26-ats.iot.us-west-2.amazonaws.com"
-   topic = "sdk/test/Python"
+   topic = "rpi_test"
 
 # parse and load command-line parameter values
    
