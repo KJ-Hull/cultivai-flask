b3dc7cbec249ec89f276f2d03b55993181480fd9
KJ-Hull <keegan.j.hull@gmail.com>
2021-01-27 10:56:34 +0200

    action push - measurement send

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ import json
 import requests
 from dotenv import load_dotenv
 import os
-from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import paho.mqtt.client as mqtt
+import ssl
 #from aws import check_bucket, create_bucket, s3_aws_init, upload_file, create_json_file 
 
 from control import set_status, get_status, get_temp, get_humid, get_moist, get_uv_light
@@ -12,13 +13,15 @@ from datetime import timedelta, datetime
 import uuid
 import time
 from request_handling import post_meas
-import argparse
 
 temp_hum_pin = 17
 moisture_pin = 5
 uv_pin = 16
 device_id = 'ef720fc0-20ca-4485-92fe-c95c67ee9307'
 measurement_id = ""
+action_type = ''
+received_dev_id = ''
+received_variable = ''
 
 def post_schedule():
     if request.method == 'POST':
@@ -56,20 +59,56 @@ def get_uv():
     json_uv= {"variable":name, "value":uv,"device_id":device_id}
     return json.dumps(json_uv)
 
-parser = argparse.ArgumentParser(description='Choosing sensor to measure.')
-parser.add_argument('sensor', help='temperature, humidity, moisture, uv')
-args = parser.parse_args()
-argument = args.sensor
+MQTT_PORT = 8883
+MQTT_KEEPALIVE_INTERVAL = 45
 
-if argument == "temperature":
-    post_meas(get_temperature())
-if argument == "humidity":
-    post_meas(get_humidity())
-if argument == "moisture":
-    post_meas(get_moisture())
-if argument == "uv":
-    post_meas(get_uv())
+env_dir = "/home/pi/device_var.env"
+load_dotenv(env_dir)
 
+
+MQTT_TOPIC = os.getenv("GET_TOPIC")
+CA_ROOT_CERT_FILE = os.getenv("CA")
+THING_CERT_FILE = os.getenv("CERT")
+THING_PRIVATE_KEY = os.getenv("PRIV_KEY")
+MQTT_ENDPOINT = os.getenv("ENDPOINT")
+
+MQTT_HOST = 'https://' + MQTT_ENDPOINT + ':8443/topics/' + MQTT_TOPIC + '?qos=1'
+
+def on_connect(mosq, obj, rc):
+    mqttc.subscribe(MQTT_TOPIC, 0)
+
+def on_message(mosq, obj, msg):
+    json_action = msg.payload
+    action_type = json_action["action_type"]
+    received_dev_id = json_action["device_id"]
+    received_variable = json_action["variable"]
+
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+    print("Subscribed to Topic: " + 
+	MQTT_MSG + " with QoS: " + str(granted_qos))
+
+mqttc = mqtt.Client()
+
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_subscribe = on_subscribe
+
+mqttc.tls_set(CA_ROOT_CERT_FILE, certfile=THING_CERT_FILE, keyfile=THING_PRIVATE_KEY, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
+
+mqttc.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL)
+
+if action_type == "measurement":
+    if received_variable == "temperature":
+        post_meas(get_temperature())
+    if received_varaible == "humidity":
+        post_meas(get_humidity())
+    if received_variable == "moisture":
+        post_meas(get_moisture())
+    if received_variable == "uv":
+        post_meas(get_uv())
+
+mqttc.loop_forever()
 
 # s3_aws_init(209, "temp", get_temperature())
    
