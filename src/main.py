40b1ecad9363252bd8a0f69c40c989d08544602b
KJ-Hull <keegan.j.hull@gmail.com>
2021-02-03 10:49:09 +0200

    test

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ from request_handling import post_meas, MQTT_action, payload_handling
 temp_hum_pin = 17
 moisture_pin = 5
 uv_pin = 16
-device_id = str(os.getenv("DEVICE_ID"))
+
 measurement_id = ""
 print(device_id)
 action_type = ''
@@ -29,6 +29,7 @@ MQTT_KEEPALIVE_INTERVAL = 45
 MQTT_MASTER_TOPIC = "MASTER"
 env_dir = "/home/pi/device_var.env"
 load_dotenv(env_dir)
+device_id = str(os.getenv("DEVICE_ID"))
 MQTT_HOST = os.getenv("THING_HOST")
 client_id = "RPI_device"   #This needs to be the device's unique ID
 MQTT_TOPIC = device_id + "/Get"
