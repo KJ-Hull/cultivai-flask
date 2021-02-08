b497bfe32a2075853fe343a5e97edf68b5e01e17
KJ-Hull <keegan.j.hull@gmail.com>
2021-02-08 08:55:56 +0200

    test

--- src/request_handling.py
+++ src/request_handling.py
@@ -56,8 +56,6 @@ def payload_handling(payload):
 
 def MQTT_action(action_type, received_variable, received_dev_id, pin):
     if action_type == "measurement":
-        print("hello")
-        print(received_variable)
         if received_variable == "temperature":
             print("hello2")
             mqtt_meas(get_temperature(pin), action_type)
